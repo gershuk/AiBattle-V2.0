9aad6e8a37bf42e1de22883a0a84ad804b67a2d0
Vladislav Gershuk <vladik.morozov.2012@mail.ru>
2022-11-23 18:27:36 +1000

    Update drawing text on image.

--- packages/engine/src/Utilities/DrawTextOnImage.ts
+++ packages/engine/src/Utilities/DrawTextOnImage.ts
@@ -1,57 +1,18 @@
-let text_title ="Overlay text";
-let imageLoader = document.getElementById('imageLoader');
-    imageLoader.addEventListener('change', handleImage, false);
-let canvas : HTMLCanvasElement = document.getElementById('imageCanvas');
-let ctx = canvas.getContext('2d');
-let img = new Image();
-img.crossOrigin="anonymous";
-
-function DrawOverlay(img) {
+export function DrawOverlay(canvas : HTMLCanvasElement,img:HTMLImageElement) {
+    const ctx = canvas.getContext('2d');
     ctx.drawImage(img,0,0);
     ctx.fillStyle = 'rgba(30, 144, 255, 0.4)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-function DrawText() {
+export function DrawText(canvas : HTMLCanvasElement, text:string) {
+    const ctx = canvas.getContext('2d');
     ctx.fillStyle = "white";
     ctx.textBaseline = 'middle';
     ctx.font = "50px 'Montserrat'";
-    ctx.fillText(text_title, 50, 50);
+    ctx.fillText(text, 50, 50);
 }
 
-function DynamicText(img) {
-  document.getElementById('name').addEventListener('keyup', function() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    DrawOverlay(img);
-    DrawText(); 
-    text_title = this.value;
-    ctx.fillText(text_title, 50, 50);
-  });
-}
-
-function handleImage(e) {
-    let reader = new FileReader();
-    let img = "";
-    let src = "";
-    reader.onload = function(event) {
-        img = new Image();
-        img.onload = function() {
-            canvas.width = img.width;
-            canvas.height = img.height;
-            ctx.drawImage(img,0,0);
-        }
-        img.src = event.target.result;
-        src = event.target.result;
-        canvas.classList.add("show");
-        DrawOverlay(img);
-        DrawText(); 
-        DynamicText(img);   
-    }
-
-    reader.readAsDataURL(e.target.files[0]); 
- 
-}
-
-function convertToImage() {
+export function convertToImage(canvas : HTMLCanvasElement) {
 	window.open(canvas.toDataURL('png'));
 }
