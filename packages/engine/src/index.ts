54db47970b65d8eff1c74e0852ce3640774809cf
Vladislav Gershuk <vladik.morozov.2012@mail.ru>
2022-11-24 17:08:49 +1000

    Add new export types.

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -1,6 +1,15 @@
+import { StaticRenderComponentParameters } from 'GameEngine/BaseComponents/RenderComponents/StaticRenderComponent'
 import { Vector2 } from 'GameEngine/BaseComponents/Vector2'
-import { GameEngine } from 'GameEngine/GameEngine'
+import { GameEngine, GameEngineParameters } from 'GameEngine/GameEngine'
 import { ImageLoader } from 'GameEngine/ResourceStorage/ImageLoader'
 import { IScene, SceneParameters } from 'GameEngine/Scene/IScene'
 
-export { GameEngine, IScene, SceneParameters, ImageLoader, Vector2 }
+export {
+	GameEngine,
+	GameEngineParameters,
+	IScene,
+	SceneParameters,
+	ImageLoader,
+	Vector2,
+	StaticRenderComponentParameters,
+}
