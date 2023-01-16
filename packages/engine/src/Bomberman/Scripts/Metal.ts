d22e5f9085894306ccab5bc5fdc4f01280244303
Vladislav Gershuk <vladik.morozov.2012@mail.ru>
2023-01-16 19:28:01 +1000

    Add metal component.

---
+++ packages/engine/src/Bomberman/Scripts/Metal.ts
@@ -0,0 +1,11 @@
+import { AbstractObjectComponent } from 'GameEngine/BaseComponents/AbstractObjectComponent'
+
+export class Metal extends AbstractObjectComponent {
+	OnOwnerInit(): void {}
+	OnDestroy(): void {}
+	OnSceneStart(): void {}
+	OnBeforeFrameRender(currentFrame: number, frameCount: number): void {}
+	OnAfterFrameRender(currentFrame: number, frameCount: number): void {}
+	OnFixedUpdate(index: number): void {}
+	OnFixedUpdateEnded(index: number): void {}
+}
