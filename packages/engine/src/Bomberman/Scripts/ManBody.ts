93824debf740e7bc85ab3aca7c21cfe2dea011ed
Vladislav Gershuk <vladik.morozov.2012@mail.ru>
2023-01-16 19:33:12 +1000

    Add man body component.

--- packages/engine/src/Bomberman/Scripts/ManBody.ts
+++ packages/engine/src/Bomberman/Scripts/ManBody.ts
@@ -4,56 +4,97 @@ import {
 } from 'GameEngine/BaseComponents/AbstractObjectComponent'
 import { DiscreteMovementComponent } from 'GameEngine/BaseComponents/DiscreteColliderSystem/DiscreteMovementComponent'
 import { Vector2 } from 'GameEngine/BaseComponents/Vector2'
+import { GameObject } from 'GameEngine/GameObject/GameObject'
 import { IGameObject } from 'GameEngine/GameObject/IGameObject'
 import { AbstractController } from 'GameEngine/UserAIRuner/AbstractController'
 import { LoadControllerFromString } from 'GameEngine/UserAIRuner/SafeEval'
 
 export class ManBody extends AbstractObjectComponent {
-	controller: AbstractController
-	movementComponent: DiscreteMovementComponent
+	OnFixedUpdateEnded(index: number): void {}
+
+	private _bombSpawnFunction: (
+		position: Vector2,
+		damage: number,
+		range: number,
+		ticksToExplosion: number
+	) => Promise<GameObject>
+	private _controller: AbstractController
+	private _movementComponent: DiscreteMovementComponent
+
 	Init(owner: IGameObject, parameters?: ManBodyParameters): void {
 		super.Init(owner, parameters)
-		if (parameters) this.controller = parameters.controller
+		if (parameters) {
+			this._controller = parameters.controller
+			this._bombSpawnFunction = parameters.bombSpawnFunction
+		}
 	}
 
 	OnOwnerInit(): void {}
 	OnDestroy(): void {}
 	OnSceneStart(): void {
-		this.movementComponent = this.owner.GetComponents(
+		this._movementComponent = this.owner.GetComponents(
 			DiscreteMovementComponent
 		)[0] as unknown as DiscreteMovementComponent
 	}
 	OnBeforeFrameRender(currentFrame: number, frameCount: number): void {}
 	OnAfterFrameRender(currentFrame: number, frameCount: number): void {}
-	OnFixedUpdate(index: number): void {
-		const command = this.controller.GetCommand({})
+	async OnFixedUpdate(index: number) {
+		const command = this._controller.GetCommand({})
 		switch (command) {
 			case 0:
-				this.movementComponent.bufferNewPosition =
-					this.movementComponent.currentPosition.Add(new Vector2(0, 1))
+				//idle
 				break
 			case 1:
-				this.movementComponent.bufferNewPosition =
-					this.movementComponent.currentPosition.Add(new Vector2(1, 0))
+				this._movementComponent.bufferNewPosition =
+					this._movementComponent.currentPosition.Add(new Vector2(0, 1))
 				break
 			case 2:
-				this.movementComponent.bufferNewPosition =
-					this.movementComponent.currentPosition.Add(new Vector2(0, -1))
+				this._movementComponent.bufferNewPosition =
+					this._movementComponent.currentPosition.Add(new Vector2(1, 0))
 				break
 			case 3:
-				this.movementComponent.bufferNewPosition =
-					this.movementComponent.currentPosition.Add(new Vector2(-1, 0))
+				this._movementComponent.bufferNewPosition =
+					this._movementComponent.currentPosition.Add(new Vector2(0, -1))
+				break
+			case 4:
+				this._movementComponent.bufferNewPosition =
+					this._movementComponent.currentPosition.Add(new Vector2(-1, 0))
+				break
+			case 5:
+				const bomb = await this._bombSpawnFunction(
+					this._movementComponent.currentPosition,
+					1,
+					1,
+					3
+				)
+				if (bomb) this._movementComponent.SetReceiver(bomb)
 				break
 			default:
-				throw 'Unknown command'
+				console.warn(`Unknown command - ${command}`)
+				break
 		}
 	}
 }
 
 export class ManBodyParameters extends ComponentParameters {
 	controller: AbstractController
-	constructor(controllerText: string) {
+	bombSpawnFunction: (
+		position: Vector2,
+		damage: number,
+		range: number,
+		ticksToExplosion: number
+	) => Promise<GameObject>
+	constructor(
+		controllerText: string,
+		bombSpawnFunction: (
+			position: Vector2,
+			damage: number,
+			range: number,
+			ticksToExplosion: number
+		) => Promise<GameObject>
+	) {
 		super()
 		this.controller = LoadControllerFromString(controllerText)
+		this.bombSpawnFunction = bombSpawnFunction
 	}
 }
