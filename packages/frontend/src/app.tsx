b28d6a29a7a7f6c8bacb37eddf4adacdc6dd7e14
Oleg <Olegoria3538@yandex.ru>
2023-02-28 22:52:00 +1000

    Add routes view.

--- packages/frontend/src/app.tsx
+++ packages/frontend/src/app.tsx
@@ -1,42 +1,17 @@
-import { useUnit } from 'effector-react'
-import { Game } from 'pages/game'
 import { SideBar } from 'features/side-bar'
-import { ControllerEditor } from 'pages/controller-editor'
-import { MapEditor } from 'pages/map-editor'
-import { Replays } from 'pages/replays'
-import {
-	$locationSearch,
-	readCodesFromLocalStorageFx,
-	readMapsFromLocalStorageFx,
-} from 'model'
+import { readCodesFromLocalStorageFx, readMapsFromLocalStorageFx } from 'model'
 import './styles.scss'
+import { RoutesView } from 'routing'
 
 export const App = () => {
 	return (
 		<div className={'app'}>
 			<SideBar />
 			<div className={'content'}>
-				<SwitchPage />
+				<RoutesView />
 			</div>
 		</div>
 	)
-}
-
-//TODO: вынести роутинг в отдельный контролер и сделать хорошо
-const SwitchPage = () => {
-	const { page = '' } = useUnit($locationSearch)
-	switch (page) {
-		case '':
-		case 'controller-editor':
-			return <ControllerEditor />
-		case 'game':
-			return <Game />
-		case 'map-editor':
-			return <MapEditor />
-		case 'replay':
-			return <Replays />
-	}
-	return null
 }
 
 readCodesFromLocalStorageFx()
