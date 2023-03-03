d21e486d9341df230b7db8986514a30ca6ed6163
Vladislav Gershuk <vladik.morozov.2012@mail.ru>
2023-03-03 19:08:39 +1000

    Added the basic engine classes.

---
+++ packages/engine/src/GameEngine/ObjectBaseType/ObjectContainer.ts
@@ -0,0 +1,43 @@
+export interface IObjectContainer<T> extends Iterable<SafeReference<T>> {
+	Add(object: T): SafeReference<T>
+	GetSafeRefsByFilter(
+		filter: (o: SafeReference<T>) => boolean
+	): SafeReference<T>[]
+	DestroyObjectsByFilter(filter: (o: SafeReference<T>) => boolean): void
+	ClearDestroyed(): void
+	Count(): number
+	AliveCount(): number
+}
+
+export class SafeReference<T> {
+	private _object: T
+
+	public get object(): T {
+		if (this._isDestroyed) throw 'Object is destroyed'
+		return this._object
+	}
+
+	protected set object(v: T) {
+		this._object = v
+	}
+
+	private _isDestroyed: boolean
+
+	public get isDestroyed(): boolean {
+		return this._isDestroyed
+	}
+
+	protected set isDestroyed(v: boolean) {
+		this._isDestroyed = v
+	}
+
+	constructor(object: T) {
+		this.object = object
+		this.isDestroyed = false
+	}
+
+	public Destroy() {
+		this.object = null
+		this.isDestroyed = true
+	}
+}
---
+++ packages/engine/src/GameEngine/ObjectBaseType/UpdatableObjectArrayContainer.ts
@@ -0,0 +1,130 @@
+import { UpdatableObject } from 'GameEngine/ObjectBaseType/UpdatableObject'
+import {
+	IObjectContainer,
+	SafeReference,
+} from 'GameEngine/ObjectBaseType/ObjectContainer'
+
+export class UpdatableObjectArrayContainer<T extends UpdatableObject>
+	implements IObjectContainer<T>
+{
+	private _version: number = 0
+	private _references: SafeReference<T>[] = []
+
+	public Count(): number {
+		return this._references.length
+	}
+
+	public get version(): number {
+		return this._version
+	}
+
+	protected set version(v: number) {
+		this._version = v
+	}
+
+	protected get references(): SafeReference<T>[] {
+		return this._references
+	}
+
+	protected set references(o: SafeReference<T>[]) {
+		this._references = o
+		this.version++
+	}
+
+	public AliveCount(): number {
+		return this.GetSafeRefsByFilter((s: SafeReference<T>): boolean => {
+			return !s.isDestroyed
+		}).length
+	}
+
+	private SortArray() {
+		this.references = this.references.sort(
+			(a: SafeReference<T>, b: SafeReference<T>): number => {
+				return (
+					(b.isDestroyed ? 0 : b.object.executionPriority) -
+					(a.isDestroyed ? 0 : a.object.executionPriority)
+				)
+			}
+		)
+	}
+
+	public Add(object: T): SafeReference<T> {
+		const safeReference: SafeReference<T> = new SafeReference<T>(object)
+		this.references.push(safeReference)
+		this.SortArray()
+		return safeReference
+	}
+
+	public GetSafeRefsByFilter(
+		filter: (s: SafeReference<T>) => boolean
+	): SafeReference<T>[] {
+		return this.references.filter(filter)
+	}
+
+	public DestroyObjectsByFilter(
+		filter: (s: SafeReference<T>) => boolean
+	): void {
+		const removedRefs = this.references.filter(filter)
+
+		for (let ref of removedRefs) {
+			ref.Destroy()
+		}
+	}
+
+	public ClearDestroyed(): void {
+		this.references = this.GetSafeRefsByFilter(
+			(s: SafeReference<T>): boolean => !s.isDestroyed
+		)
+		this.SortArray()
+	}
+
+	[Symbol.iterator](): Iterator<SafeReference<T>, any, undefined> {
+		return new UpdatableObjectContainerIterator(
+			this._references,
+			() => this.version
+		)
+	}
+}
+
+export class UpdatableObjectContainerIterator<T extends UpdatableObject>
+	implements Iterator<SafeReference<T>>
+{
+	private _index: number
+	private _version: number
+	private _references: SafeReference<T>[]
+	private _getCurrentVersionNumber: () => number
+
+	constructor(
+		references: SafeReference<T>[],
+		getCurrentVersionNumber: () => number
+	) {
+		this._references = references
+		this._index = 0
+		this._getCurrentVersionNumber = getCurrentVersionNumber
+		this._version = this._getCurrentVersionNumber()
+	}
+
+	next(...args: [] | [undefined]): IteratorResult<SafeReference<T>, any> {
+		// if (this._version != this._getCurrentVersionNumber())
+		// 	throw new Error('Container version was changed')
+
+		this.SkipToFirstAliveOrEnd()
+
+		const result: IteratorResult<SafeReference<T>, any> = {
+			done: this._index >= this._references.length,
+			value:
+				this._index < this._references.length
+					? this._references[this._index]
+					: undefined,
+		}
+
+		this._index++
+
+		return result
+	}
+
+	private SkipToFirstAliveOrEnd() {
+		while (this._index < length && this._references[this._index].isDestroyed)
+			this._index++
+	}
+}
