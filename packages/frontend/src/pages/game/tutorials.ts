a559f43327aea980b7544c0bbdd492022ae6cc0c
Oleg <Olegoria3538@yandex.ru>
2023-03-20 13:50:51 +1000

    Add translation tutorial game start.

--- packages/frontend/src/pages/game/tutorials.ts
+++ packages/frontend/src/pages/game/tutorials.ts
@@ -1,18 +1,41 @@
 import { sample } from 'effector'
 import { $activeGame } from 'features/game-controller'
+import { createTranslation } from 'libs'
 import { createTutorial } from 'libs/tutorial'
 import { createTutorialPanel } from 'ui'
+
+const { getTranslationItem } = createTranslation({
+	ru: {
+		startGame: 'Запуск игры',
+		configGame: 'Здесь ты конфигурируешь игру',
+		activeStart:
+			'Как выберешь карту и настроишь ботов кнопка старта активируется',
+		startInfo:
+			'Ты можешь запустить автоматическую игру ботов или же контролировать каждый их ход',
+		zoom: 'Ты можешь уменьшать/увеличивать масштаб игры колёсиком мышки при нажатом ctrl. А также перемещать карту захватом мышки.',
+		facilities: 'Удобства',
+	},
+	en: {
+		startGame: 'Game start',
+		configGame: 'Here you configure the game',
+		activeStart:
+			'When you select a map and set up bots, the start button is activated',
+		startInfo:
+			'You can start the bots to play automatically or control their every move',
+		zoom: 'You can zoom in/zoom out of the game with the mouse wheel while pressing ctrl. You can also move the map with the mouse.',
+		facilities: 'Facilities',
+	},
+})
 
 export const tutorialGameSetting = createTutorial({
 	steps: [
 		{
-			message: 'Здесь ты конфигурируешь игру',
-			title: 'Запуск игры',
+			message: () => getTranslationItem('configGame'),
+			title: () => getTranslationItem('startGame'),
 		},
 		{
-			message:
-				'Как выберешь карту и настроишь ботов кнопка старта активируется',
-			title: 'Запуск игры',
+			message: () => getTranslationItem('activeStart'),
+			title: () => getTranslationItem('startGame'),
 			element: () =>
 				document.querySelector(
 					'.game-controller .game-controller-footer .btn-toggle-game'
@@ -28,16 +51,14 @@ export const tutorialGameSetting = createTutorial({
 export const tutorialGameStart = createTutorial({
 	steps: [
 		{
-			message:
-				'Ты можешь запустить автоматическую игру ботов или же контролировать каждый их ход',
-			title: 'Запуск игры',
+			message: () => getTranslationItem('startInfo'),
+			title: () => getTranslationItem('startGame'),
 			element: () => document.querySelector('.game-controller-footer'),
 			viewPosition: 'top-right',
 		},
 		{
-			message:
-				'Ты можешь уменьшать/увеличивать масштаб игры колёсиком мышки при нажатом ctrl. А также перемещать карту захватом мышки.',
-			title: 'Удобства',
+			message: () => getTranslationItem('zoom'),
+			title: () => getTranslationItem('facilities'),
 			element: () => document.querySelector('.awesome-canvas-game canvas'),
 			viewPosition: 'left',
 		},
