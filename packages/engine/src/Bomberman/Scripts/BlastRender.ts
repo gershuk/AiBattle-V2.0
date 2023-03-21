d27c6aa34470020bc0c8369e42dc87e4e16ced43
Vladislav Gershuk <vladik.morozov.2012@mail.ru>
2023-03-21 23:13:34 +1000

    Optimize BlastRender.

--- packages/engine/src/Bomberman/Scripts/BlastRender.ts
+++ packages/engine/src/Bomberman/Scripts/BlastRender.ts
@@ -6,19 +6,6 @@ import { Vector2 } from 'GameEngine/BaseComponents/Vector2'
 import { IGameObject } from 'GameEngine/GameObject/IGameObject'
 
 export class BlastRender extends StaticImageRenderComponent {
-	private _turnToExplosion: number
-
-	Init(
-		gameObject: IGameObject,
-		parameters?: StaticRenderComponentParameters
-	): void {
-		super.Init(gameObject, parameters)
-		if (gameObject) {
-			this._turnToExplosion =
-				(gameObject.scene.turnIndex ? gameObject.scene.turnIndex : 1) + 1
-		}
-	}
-
 	OnBeforeFrameRender(currentFrame: number, frameCount: number): void {
 		const renderScale = (frameCount - currentFrame + 1) / frameCount
 		const offset = (currentFrame + 1) / (frameCount * 2)
@@ -26,11 +13,9 @@ export class BlastRender extends StaticImageRenderComponent {
 		this.offset = new Vector2(offset, offset)
 	}
 
-	OnFixedUpdate(index: number): void {
-		if (index === this._turnToExplosion) {
-			this.gameObject.scene.RemoveGameObjectsByFilter(
-				r => this.gameObject === r.object
-			)
-		}
+	OnFixedUpdateEnded(index: number): void {
+		this.gameObject.scene.RemoveGameObjectsByFilter(
+			r => this.gameObject === r.object
+		)
 	}
 }
